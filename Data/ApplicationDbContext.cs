b5f334fe2c2c67c5f5155c109252cd3d846d3b0e
randethar <randethar@gmail.com>
2026-01-22 15:08:23 +0000

    Create admin dashboard layout

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,7 +7,10 @@ namespace kASHOP.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=.;Database=KAShopDB;TrustServerCertificate=True;Trusted_Connection=True;");
+            //database production
+            optionsBuilder.UseSqlServer("Server=db38630.public.databaseasp.net; Database=db38630; User Id=db38630; Password=yZ-79eH@!D4s; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True;  ");
+            //Development Database
+            //optionsBuilder.UseSqlServer("Server=.;Database=KAShopDB;TrustServerCertificate=True;Trusted_Connection=True;");
         }
     }
 }
